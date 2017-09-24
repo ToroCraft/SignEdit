cd9701f5209eaf6d434cd69145864f28cba115ec
Charles Howard <frodare@gmail.com>
2017-09-24 09:32:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SignEdit Minecraft Mod
+
+## Development Environment Setup
+
+```
+git clone git@github.com:ToroCraft/SignEdit.git
+cd SignEdit
+gradle setupDecompWorkspace
+```
+
+To setup an Intellij environment:
+```
+gradle idea
+```
+
+To setup an Eclipse environment:
+```
+gradle eclipse
+```
---
+++ src/main/java/net/torocraft/nemesissystem/SignEdit.java
@@ -0,0 +1,39 @@
+package net.torocraft.nemesissystem;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntitySign;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = SignEdit.MODID, version = SignEdit.VERSION, name = SignEdit.MODNAME)
+public class SignEdit {
+
+  public static final String MODID = "signedit";
+  public static final String VERSION = "1.12.1-2";
+  public static final String MODNAME = "SignEdit";
+
+  @EventBusSubscriber
+  public static class DiscoveryHandler {
+
+    @SubscribeEvent
+    public static void editSign(RightClickBlock event) {
+      BlockPos pos = new BlockPos(event.getHitVec());
+      IBlockState state = event.getWorld().getBlockState(pos);
+      if (state.getBlock() != Blocks.WALL_SIGN && state.getBlock() != Blocks.STANDING_SIGN) {
+        return;
+      }
+      EntityPlayer player = event.getEntityPlayer();
+      TileEntity tileentity = event.getWorld().getTileEntity(pos);
+      if (tileentity instanceof TileEntitySign) {
+        player.openEditSign((TileEntitySign) tileentity);
+      }
+    }
+
+  }
+}
