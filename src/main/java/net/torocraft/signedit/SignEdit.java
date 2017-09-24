f3c832fa83885dd40c4cb2bd1de95c6c5d4bf12c
Charles Howard <frodare@gmail.com>
2017-09-24 14:42:47 -0400

    backport to 1.11.2

--- src/main/java/net/torocraft/signedit/SignEdit.java
+++ src/main/java/net/torocraft/signedit/SignEdit.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class SignEdit {
 
   public static final String MODID = "signedit";
-  public static final String VERSION = "1.12.2-1";
+  public static final String VERSION = "1.11.2-1";
   public static final String MODNAME = "SignEdit";
 
   @EventBusSubscriber
