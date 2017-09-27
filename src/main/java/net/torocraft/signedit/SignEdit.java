e6637fea1661f47c1c57835c7b8d99aeaf505330
Charles Howard <frodare@gmail.com>
2017-09-27 19:35:47 -0400

    version step

--- src/main/java/net/torocraft/signedit/SignEdit.java
+++ src/main/java/net/torocraft/signedit/SignEdit.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class SignEdit {
 
   public static final String MODID = "signedit";
-  public static final String VERSION = "1.12.2-3";
+  public static final String VERSION = "1.12.2-4";
   public static final String MODNAME = "SignEdit";
 
   @EventHandler
